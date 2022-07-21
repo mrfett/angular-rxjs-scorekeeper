9563d025a28b75dd12d9f9a613dd90589c2bca87
B.J. Self <bj.self@wwt.com>
2022-07-21 09:28:10 -0500

    updated styles to layout better on different sized screens

--- src/app/bout.ts
+++ src/app/bout.ts
@@ -1,6 +1,7 @@
 import { Fencer } from "./fencer";
 
 export class Bout {
+  weapon: string = "foil";
   timeLeft: number = 3 * 60 * 1000;
   status: string = "paused";
   fencerA: Fencer = new Fencer();
